a63a1526ac57cacaf301953d994d4b6c89d7e882
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-14 19:04:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+# require 'pry'
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |style, hash|
+    hash.each do |lang, attributes|
+      attributes.each do |type, type_value|
+        new_hash[lang] = {type => type_value}
+        new_hash[lang][:style] = [style]
+
+      end
+    end
+  end
+  new_hash
+  # binding.pry
 end
