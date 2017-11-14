bcb7ba0a7bd648c3f4fdc8e428845ae8e53caa46
Jerry Bennett <jerrysbusiness@gmail.com>
2017-11-14 19:58:44 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,11 +5,11 @@ def reformat_languages(languages)
     hash.each do |lang, attributes|
       attributes.each do |k, v|
         new_hash[lang] = {:type => v, :style => [style]}
-        # if new_hash[lang][:style] == nil
-        #   new_hash[lang][:style] = [style]
-        # else
-        #   new_hash[lang][:style] << style
-        # end
+        if new_hash[lang][:style] == nil
+          new_hash[lang][:style] = [style]
+        else
+          new_hash[lang][:style] << style
+        end
       end
     end
   end
